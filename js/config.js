ea75c9c77d0637ec709d21ddec11fc190f86242b
lovelymyanmar <96009299+lovelymyanmar@users.noreply.github.com>
2021-12-14 22:29:07 +0630

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,16 @@
+let site_detail = {
+    'site_name': {
+        'tc': 'LovelyMyanmar',
+        'url': 'https://lovelymyanmar.github.io'
+    },
+    'site_home': {
+        'tc': 'Home',
+        'url': 'https://lovelymyanmar.github.io/springmoni'
+    },
+    'products': {
+        'tc': 'Our Products',
+        'url': 'https://lovelymyanmar.github.io/springmoni/products/index.html'
+    }
+}
+
+export {site_detail}
