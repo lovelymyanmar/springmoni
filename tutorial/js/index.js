3886afc718cdec5e167bd8ce1e84c575f69c0422
lovelymyanmar <96009299+lovelymyanmar@users.noreply.github.com>
2021-12-16 22:51:25 +0630

    Update index.js

--- tutorial/js/index.js
+++ tutorial/js/index.js
@@ -1,6 +1,6 @@
-import { openMenu, closeMenu } from "./module/menu.js";
+import { openMenu, closeMenu } from "https://lovelymyanmar.github.io/springmoni/tutorial/js/menu.js";
 openMenu(); closeMenu();
 
-import { articles } from "./module/articles.js";
+import { articles } from "https://lovelymyanmar.github.io/springmoni/tutorial/js/articles.js";
 let blog = 'springcondor'; // https://springcondor.blogspot.com
 articles(blog);
