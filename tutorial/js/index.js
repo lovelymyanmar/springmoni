cc2ac7cc52dae2c6b7c10a9c160d11d614c646e8
lovelymyanmar <96009299+lovelymyanmar@users.noreply.github.com>
2021-12-16 22:44:34 +0630

    Create index.js

---
+++ tutorial/js/index.js
@@ -0,0 +1,6 @@
+import { openMenu, closeMenu } from "./module/menu.js";
+openMenu(); closeMenu();
+
+import { articles } from "./module/articles.js";
+let blog = 'springcondor'; // https://springcondor.blogspot.com
+articles(blog);
